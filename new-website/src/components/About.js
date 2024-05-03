1530aa2c4161322eba86c5c2f33c235abcb067f6
Javier Esteban <javierethomas@gmail.com>
2024-05-02 21:06:35 -0400

    Update PP

--- new-website/src/components/About.js
+++ new-website/src/components/About.js
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded-full"
             alt="hero"
-            src="profile.png"
+            src="profile1.png"
           />
         </div>
       </div>
